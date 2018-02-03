f066e7bf3c7eb13d5e5765b3d95cd8e58e16572c
John Hotterbeekx <john.online@hotterbeekx.nl>
2018-02-03 17:30:23 +0100

    Added first extension methods

---
+++ SeleniumExtensions/WebElementExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using OpenQA.Selenium;
+
+namespace SeleniumExtensions {
+  public static class WebElementExtensions {
+    public static string InnerHtml(this IWebElement element) {
+      return element.GetAttribute("innerHTML");
+    }
+
+    public static string OuterHtml(this IWebElement element) {
+      return element.GetAttribute("OuterHtml");
+    }
+
+    public static string InnerText(this IWebElement element) {
+      return element.GetAttribute("innerText");
+    }
+
+    public static string Class(this IWebElement element) {
+      return element.GetAttribute("class");
+    }
+
+    public static bool HasClass(this IWebElement element, string className) {
+      return element.GetAttribute("class")?.Split(' ').Contains(className) ?? false;
+    }
+  }
+}
